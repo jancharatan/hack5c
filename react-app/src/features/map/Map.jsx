216b3976e62576b4b9489f56340780c23dc00c7f
jancharatan <52763246+jancharatan@users.noreply.github.com>
2020-11-06 19:38:22 -0800

    Made map prettier.

--- react-app/src/features/map/Map.jsx
+++ react-app/src/features/map/Map.jsx
@@ -7,9 +7,39 @@ const geoUrl =
 const Map = () => (
   <div className="w-50 h-100">
     <div>
-      <ComposableMap projection="geoAlbersUsa" width={800} height={600}>
+      <ComposableMap projection="geoAlbersUsa">
         <Geographies geography={geoUrl}>
-          {({ geographies }) => geographies.map((geo) => <Geography key={geo.rsmKey} geography={geo} fill="#F00" />)}
+          {
+            ({ geographies }) =>
+              geographies.map((geo) => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  fill="#000"
+                  style={{
+                    default: {
+                      fill: '#ECEFF1',
+                      stroke: '#607D8B',
+                      strokeWidth: 0.75,
+                      outline: 'none',
+                    },
+                    hover: {
+                      fill: '#607D8B',
+                      stroke: '#607D8B',
+                      strokeWidth: 0.75,
+                      outline: 'none',
+                    },
+                    pressed: {
+                      fill: '#FF5722',
+                      stroke: '#607D8B',
+                      strokeWidth: 0.75,
+                      outline: 'none',
+                    },
+                  }}
+                />
+              ))
+            // eslint-disable-next-line react/jsx-curly-newline
+          }
         </Geographies>
       </ComposableMap>
     </div>
