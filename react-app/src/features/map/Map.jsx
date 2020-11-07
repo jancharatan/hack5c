f63f35413cd50a994befe49c85f22617e04075ed
jancharatan <52763246+jancharatan@users.noreply.github.com>
2020-11-06 19:13:06 -0800

    Added county map.

--- react-app/src/features/map/Map.jsx
+++ react-app/src/features/map/Map.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { ComposableMap, Geographies, Geography, ZoomableGroup } from 'react-simple-maps';
 
-const Map = () => <div className="w-100 h-100 bg-primary">Map Placeholder</div>;
+const geoUrl =
+  'https://raw.githubusercontent.com/deldersveld/topojson/master/countries/united-states/us-albers-counties.json';
 
+const Map = () => (
+  <div style={{ width: '90%' }}>
+    <ComposableMap>
+      <ZoomableGroup>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) => geographies.map((geo) => <Geography key={geo.rsmKey} geography={geo} />)}
+        </Geographies>
+      </ZoomableGroup>
+    </ComposableMap>
+  </div>
+);
 export default Map;
