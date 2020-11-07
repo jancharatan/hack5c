4c9fe9d6b108fa3debfd0cd83dfea9b39416e129
David Charatan <david.charatan@gmail.com>
2020-11-07 14:20:57 -0500

    Make the website ask for a link from the server.

--- react-app/src/environment.js
+++ react-app/src/environment.js
@@ -3,3 +3,5 @@ require('dotenv').config();
 
 // eslint-disable-next-line import/prefer-default-export
 export const getBaseUrl = () => process.env.REACT_APP_BASE_URL ?? 'http://localhost:5000';
+
+export const getFrontendBaseUrl = () => process.env.REACT_APP_FRONTEND_BASE_URL ?? 'http://localhost:3000';
