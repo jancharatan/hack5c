c4d6fe3861fb9abd190deb3f0c261a8f3941bead
lmaskati <liyaan_maskati@brown.edu>
2020-11-07 00:17:32 -0500

    constraint stuff

--- flask-server/data.py
+++ flask-server/data.py
@@ -4,8 +4,34 @@ import csv
 # load data
 counties = "us-counties.csv"
 states = "us-states.csv"
+demographics = "demographic.csv"
 counties = pd.read_csv(counties)
 states = pd.read_csv(states)
+dems = pd.read_csv(
+    demographics,
+    usecols=[
+        "State",
+        "County",
+        "TotalPop",
+        "Men",
+        "Women",
+        "Hispanic",
+        "White",
+        "Black",
+        "Native",
+        "Asian",
+        "Pacific",
+        "Income",
+    ],
+)
+dems = dems.rename(columns={"State": "state", "County": "county"})
+
+# merge demographic info w counties
+counties_merged = pd.merge(dems, counties, how="left", on=["state", "county"])
+counties_merged = counties_merged.dropna(subset=["fips"])
+# merge demographic info w states
+states_merged = pd.merge(dems, states, how="left", on=["state"])
+states_merged = states_merged.dropna(subset=["fips"])
 
 # check to see if loading correctly:
 # counties_test = counties.head(10)
@@ -51,3 +77,46 @@ def get_date_per_state(my_date, my_state_fip):
 
 # print("***")
 # print(get_date_per_state('2020-04-25', 41))
+
+# constraints keys should be Asian, White ... etc. Values should be [min, max]
+def getMatchingFips(constraints, toggle):
+    if toggle == "county":
+        left_merged = counties_merged
+    else:
+        left_merged = states_merged
+
+    Men = constraints.get("Men", [float("-inf"), float("inf")])
+    Women = constraints.get("Women", [float("-inf"), float("inf")])
+    Hispanic = constraints.get("Hispanic", [float("-inf"), float("inf")])
+    White = constraints.get("White", [float("-inf"), float("inf")])
+    Black = constraints.get("Black", [float("-inf"), float("inf")])
+    Native = constraints.get("Native", [float("-inf"), float("inf")])
+    Asian = constraints.get("Asian", [float("-inf"), float("inf")])
+    Pacific = constraints.get("Pacific", [float("-inf"), float("inf")])
+    Income = constraints.get("Income", [float("-inf"), float("inf")])
+
+    sorted_data = left_merged[
+        (left_merged.Men >= Men[0])
+        & (left_merged.Men <= Men[1])
+        & (left_merged.Women >= Women[0])
+        & (left_merged.Women <= Women[1])
+        & (left_merged.Hispanic >= Hispanic[0])
+        & (left_merged.Hispanic <= Hispanic[1])
+        & (left_merged.White >= White[0])
+        & (left_merged.White <= White[1])
+        & (left_merged.Black >= Black[0])
+        & (left_merged.Black <= Black[1])
+        & (left_merged.Native >= Native[0])
+        & (left_merged.Native <= Native[1])
+        & (left_merged.Asian >= Asian[0])
+        & (left_merged.Asian <= Asian[1])
+        & (left_merged.Pacific >= Pacific[0])
+        & (left_merged.Pacific <= Pacific[1])
+        & (left_merged.Income >= Income[0])
+        & (left_merged.Income <= Income[1])
+    ]
+
+    return list(set(sorted_data["fips"]))
+
+
+# print(getMatchingFips({'Asian' : [10, 20], 'White' : [30, 100]}, 'county'))
