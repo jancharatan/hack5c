31c01d4d63811bf4ab7bb9101e0564a25fcc28a6
David Charatan <david.charatan@gmail.com>
2020-11-07 12:28:25 -0500

    Remove header.

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import React from 'react';
 import { Provider } from 'react-redux';
-import Header from './features/header/Header';
 import Main from './features/main/Main';
 import rootReducer from './features/redux/rootReducer';
 
@@ -13,7 +12,6 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App w-100 h-100 d-flex flex-column">
-        <Header />
         <div className="d-flex flex-grow-1 overflow-hidden">
           <Main />
         </div>
