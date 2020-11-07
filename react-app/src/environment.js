c73dcc87159c84dcd88ee655d90f95f8dc344595
David Charatan <david.charatan@gmail.com>
2020-11-07 14:26:17 -0500

    Add missing file.

--- react-app/src/environment.js
+++ react-app/src/environment.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line import/no-commonjs
 require('dotenv').config();
 
-// eslint-disable-next-line import/prefer-default-export
 export const getBaseUrl = () => process.env.REACT_APP_BASE_URL ?? 'http://localhost:5000';
 
 export const getFrontendBaseUrl = () => process.env.REACT_APP_FRONTEND_BASE_URL ?? 'http://localhost:3000';
