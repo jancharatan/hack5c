3b2da81c3b8ea10498f41da9f54b9318eb769e62
David Charatan <david.charatan@gmail.com>
2020-11-06 22:07:13 -0500

    Add POST route example.

--- flask-server/server.py
+++ flask-server/server.py
@@ -10,3 +10,9 @@ cors = CORS(app, resources={r"*": {"origins": "*"}})
 @app.route("/hello", methods=["GET"])
 def say_hello():
     return "Hello"
+
+
+@app.route("/get-state-data", methods=["POST"])
+def example_post_route():
+    state = request.json["state"]
+    return {"message": f"You asked for the state {state}!"}
