feab37c39788ca6f83cf616b4b1aacca88084764
jancharatan <52763246+jancharatan@users.noreply.github.com>
2020-11-07 12:07:52 -0800

    Made map key dynamic.

--- react-app/src/features/map/MapKey.jsx
+++ react-app/src/features/map/MapKey.jsx
@@ -1,42 +1,40 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { scaleLinear } from 'd3-scale';
+import { getMaximums } from './processFipsData';
 
 const MapKey = () => {
-  const colorScaleCases = scaleLinear().domain([0, 300000]).range(['#ffedea', '#ff5233']);
-  const colorScaleDeaths = scaleLinear().domain([0, 18000]).range(['#BFBFFF', '#0000FF']);
+  const fipsData = useSelector((state) => state.dataSlice.dataByFips);
   const selectedMapType = useSelector((state) => state.mapSlice.casesNoDeaths);
+  if (!fipsData) {
+    return <div>No data was sent from the server!</div>;
+  }
+  const [maxCases, maxDeaths] = getMaximums(fipsData);
+  const colorScaleCases = scaleLinear().domain([0, maxCases]).range(['#ffedea', '#ff5233']);
+  const colorScaleDeaths = scaleLinear().domain([0, maxDeaths]).range(['#BFBFFF', '#0000FF']);
+
+  const colorFunction = selectedMapType ? colorScaleCases : colorScaleDeaths;
+  const maxValue = selectedMapType ? maxCases : maxDeaths;
+  const text = selectedMapType ? 'Cases' : 'Deaths';
+
+  const numGradations = 5;
+  const content = [];
+  for (let i = 0; i < numGradations; i += 1) {
+    const value = i * (maxValue / (numGradations - 1));
+    content.push(
+      <>
+        <div className="m-2" style={{ width: '10px', height: '10px', backgroundColor: colorFunction(value) }} />
+        <div>{Math.round(value * 100000) / 100}</div>
+      </>
+    );
+  }
+
   return (
     <div className="mx-5 px-2 border rounded" style={{ backgroundColor: 'white' }}>
-      {selectedMapType ? (
-        <div className="d-flex flex-row">
-          <div style={{ fontWeight: 'bold' }}>Cases</div>
-          <div className="m-2" style={{ width: '10px', height: '10px', backgroundColor: colorScaleCases(0) }} />
-          <div style={{ fontWeight: 'light' }}> 0 </div>
-          <div className="m-2" style={{ width: '10px', height: '10px', backgroundColor: colorScaleCases(75000) }} />
-          <div style={{ fontWeight: 'light' }}> 75000 </div>
-          <div className="m-2" style={{ width: '10px', height: '10px', backgroundColor: colorScaleCases(150000) }} />
-          <div style={{ fontWeight: 'light' }}> 150000 </div>
-          <div className="m-2" style={{ width: '10px', height: '10px', backgroundColor: colorScaleCases(225000) }} />
-          <div style={{ fontWeight: 'light' }}> 225000 </div>
-          <div className="m-2" style={{ width: '10px', height: '10px', backgroundColor: colorScaleCases(300000) }} />
-          <div style={{ fontWeight: 'light' }}> 300000 </div>
-        </div>
-      ) : (
-        <div className="d-flex flex-row">
-          <div style={{ fontWeight: 'bold' }}>Deaths</div>
-          <div className="m-2" style={{ width: '10px', height: '10px', backgroundColor: colorScaleDeaths(0) }} />
-          <div style={{ fontWeight: 'light' }}> 0 </div>
-          <div className="m-2" style={{ width: '10px', height: '10px', backgroundColor: colorScaleDeaths(4500) }} />
-          <div style={{ fontWeight: 'light' }}> 4500 </div>
-          <div className="m-2" style={{ width: '10px', height: '10px', backgroundColor: colorScaleDeaths(9000) }} />
-          <div style={{ fontWeight: 'light' }}> 9000 </div>
-          <div className="m-2" style={{ width: '10px', height: '10px', backgroundColor: colorScaleDeaths(13500) }} />
-          <div style={{ fontWeight: 'light' }}> 13500 </div>
-          <div className="m-2" style={{ width: '10px', height: '10px', backgroundColor: colorScaleDeaths(18000) }} />
-          <div style={{ fontWeight: 'light' }}> 18000 </div>
-        </div>
-      )}
+      <div className="d-flex flex-row" style={{ fontWeight: 'light' }}>
+        <div style={{ fontWeight: 'bold' }}>{text}</div>
+        {content}
+      </div>
     </div>
   );
 };
