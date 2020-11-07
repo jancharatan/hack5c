194b32916bd6aef25ec5b36376d87853d39b99aa
David Charatan <david.charatan@gmail.com>
2020-11-06 22:43:37 -0500

    Add Redux stuff.

--- react-app/src/features/header/Header.jsx
+++ react-app/src/features/header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 
-const Header = () => (
-  <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">5C Hacks!</Navbar.Brand>
-  </Navbar>
-);
+const Header = () => {
+  const selectedCountyFips = useSelector((state) => state.mapSlice.selectedCountyFips);
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">{`5C Hacks! Selected FIPS: ${selectedCountyFips}`}</Navbar.Brand>
+    </Navbar>
+  );
+};
 
 export default Header;
--- react-app/src/features/map/Map.jsx
+++ react-app/src/features/map/Map.jsx
@@ -1,48 +1,54 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
+import { setSelectedCountyFips } from './mapSlice';
 
 const geoUrl =
   'https://raw.githubusercontent.com/deldersveld/topojson/master/countries/united-states/us-albers-counties.json';
 
-const Map = () => (
-  <div className="w-50 h-100">
-    <div>
-      <ComposableMap projection="geoAlbersUsa">
-        <Geographies geography={geoUrl}>
-          {
-            ({ geographies }) =>
-              geographies.map((geo) => (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  fill="#000"
-                  style={{
-                    default: {
-                      fill: '#ECEFF1',
-                      stroke: '#607D8B',
-                      strokeWidth: 0.75,
-                      outline: 'none',
-                    },
-                    hover: {
-                      fill: '#607D8B',
-                      stroke: '#607D8B',
-                      strokeWidth: 0.75,
-                      outline: 'none',
-                    },
-                    pressed: {
-                      fill: '#FF5722',
-                      stroke: '#607D8B',
-                      strokeWidth: 0.75,
-                      outline: 'none',
-                    },
-                  }}
-                />
-              ))
-            // eslint-disable-next-line react/jsx-curly-newline
-          }
-        </Geographies>
-      </ComposableMap>
+const Map = () => {
+  const dispatch = useDispatch();
+  return (
+    <div className="w-50 h-100">
+      <div>
+        <ComposableMap projection="geoAlbersUsa">
+          <Geographies geography={geoUrl}>
+            {
+              ({ geographies }) =>
+                geographies.map((geo) => (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    fill="#000"
+                    style={{
+                      default: {
+                        fill: '#ECEFF1',
+                        stroke: '#607D8B',
+                        strokeWidth: 0.75,
+                        outline: 'none',
+                      },
+                      hover: {
+                        fill: '#607D8B',
+                        stroke: '#607D8B',
+                        strokeWidth: 0.75,
+                        outline: 'none',
+                      },
+                      pressed: {
+                        fill: '#FF5722',
+                        stroke: '#607D8B',
+                        strokeWidth: 0.75,
+                        outline: 'none',
+                      },
+                    }}
+                    onClick={() => dispatch(setSelectedCountyFips(geo.id))}
+                  />
+                ))
+              // eslint-disable-next-line react/jsx-curly-newline
+            }
+          </Geographies>
+        </ComposableMap>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default Map;
