b95eaf08cbf85a3d956e728cee356063ed6f32aa
David Charatan <david.charatan@gmail.com>
2020-11-08 00:48:08 -0500

    Update config

--- react-app/src/environment.js
+++ react-app/src/environment.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 
 export const getBaseUrl = () => process.env.REACT_APP_BASE_URL ?? 'https://covid-dash-api2.azurewebsites.net';
 
-export const getFrontendBaseUrl = () => process.env.REACT_APP_FRONTEND_BASE_URL ?? 'http://localhost:3000';
+export const getFrontendBaseUrl = () =>
+  process.env.REACT_APP_FRONTEND_BASE_URL ?? 'https://covid-dash.azurewebsites.net';
 
 export const START_DAY = '2020-11-05';
 
